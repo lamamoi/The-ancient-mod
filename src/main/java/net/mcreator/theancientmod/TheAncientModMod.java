fba2960795343d9f0a77bd40dce0015c05112ff7
lamamoi <123@192.168.0.117>
2022-06-07 15:41:52 +0200

    removed sexlore

--- src/main/java/net/mcreator/theancientmod/TheAncientModMod.java
+++ src/main/java/net/mcreator/theancientmod/TheAncientModMod.java
@@ -26,8 +26,6 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.FriendlyByteBuf;
 
-import net.mcreator.theancientmod.init.TheAncientModModBiomes;
-
 import java.util.function.Supplier;
 import java.util.function.Function;
 import java.util.function.BiConsumer;
@@ -44,8 +42,6 @@ public class TheAncientModMod {
 	public TheAncientModMod() {
 
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
-		TheAncientModModBiomes.REGISTRY.register(bus);
 
 	}
 
